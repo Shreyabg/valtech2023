65807878ba2371ac56ab734ccb194d25b93d53d2
Shreyabg <shreyabggowda@gmail.com>
2023-10-31 12:11:10 +0530

    commiting git maven

--- first-maven-project/src/test/java/com/valtech/training/first_maven_project/AppTest.java
+++ first-maven-project/src/test/java/com/valtech/training/first_maven_project/AppTest.java
@@ -15,6 +15,13 @@ public class AppTest
      *
      * @param testName name of the test case
      */
+	
+	
+	public void testsayHello() {
+		
+		App app=new App();
+		assertEquals("Hello World", app.sayHello());
+	}
     public AppTest( String testName )
     {
         super( testName );
